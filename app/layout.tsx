40ae953996cf33ec1f8aa8410809041b717ed1f5
sashen'ka <oleksandr9german@gmail.com>
2026-09-16 21:02:26 +0300

    Load knowledge base styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import './content.css';
 import './mobile.css';
 import './process.css';
 import './layout-overrides.css';
+import './blog-knowledge.css';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://zahidalexbur.com.ua'),
