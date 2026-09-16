a6b61a257b64de303c0811750dfc718c88e33bbf
sashen'ka <oleksandr9german@gmail.com>
2026-09-16 18:39:58 +0300

    Enable high-quality responsive image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  images: {
+    qualities: [90],
+    formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 604800,
+  },
 };
 
 export default nextConfig;
