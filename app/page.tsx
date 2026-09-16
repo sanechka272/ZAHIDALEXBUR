8d62e51bfe81c4da485de5b2c7702de3943c8bb3
sashen'ka <oleksandr9german@gmail.com>
2026-09-16 16:30:02 +0300

    Force static generation for homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import LandingPage from '@/components/LandingPage';
 
+export const dynamic = 'force-static';
+export const revalidate = false;
+
 export default function Home() {
   return <LandingPage />;
 }
